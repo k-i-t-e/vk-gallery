8aa38cf036ce6080093c6781a86b50e5f6e2067f
Mikhail_Miroliubov <mike.miroliubov@gmail.com>
2018-10-05 18:36:35 +0300

    UI: viewer modal in progress

---
+++ client/src/app/gallery/image-modal/image-modal.component.ts
@@ -0,0 +1,19 @@
+import {Component, Input, OnInit} from '@angular/core';
+import {NgbActiveModal} from "@ng-bootstrap/ng-bootstrap";
+
+@Component({
+  selector: 'app-image-modal',
+  templateUrl: './image-modal.component.html',
+  styleUrls: ['./image-modal.component.css']
+})
+export class ImageModalComponent implements OnInit {
+
+  @Input() imageUrl;
+
+  constructor(public activeModal: NgbActiveModal) { }
+
+  ngOnInit() {
+    console.log(this.imageUrl)
+  }
+
+}
