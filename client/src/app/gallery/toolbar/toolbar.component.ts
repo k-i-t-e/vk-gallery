95e5182731e3cafc3589ba0556b9a490c9df44db
Mikhail Miroliubov <mike.miroliubov@gmail.com>
2018-10-08 00:30:18 +0300

    UI: Fixed toolbar to show current active tab

--- client/src/app/gallery/toolbar/toolbar.component.ts
+++ client/src/app/gallery/toolbar/toolbar.component.ts
@@ -1,6 +1,11 @@
 import {Component, OnInit} from '@angular/core';
-import {AuthService} from "../service/auth/auth.service";
-import {Router} from "@angular/router";
+import {AuthService} from '../service/auth/auth.service';
+import {Router} from '@angular/router';
+
+const TABS = {
+  GALLERY: '/gallery',
+  GROUPS: '/groups'
+};
 
 @Component({
   selector: 'app-toolbar',
@@ -8,14 +13,22 @@ import {Router} from "@angular/router";
   styleUrls: ['./toolbar.component.css']
 })
 export class ToolbarComponent implements OnInit {
-
+  activeTab = 'GALLERY';
   constructor(private authService: AuthService, private router: Router) { }
 
   ngOnInit() {
+    this.activeTab = Object.entries(TABS).filter(e => e[1] === this.router.url)[0][0]
   }
 
   logout() {
     this.authService.logout().subscribe(ignored => this.router.navigateByUrl('/login'))
   }
 
+  public tabSelect(tab: string) {
+    this.router.navigateByUrl(TABS[tab]).then(() => this.activeTab = tab)
+  }
+
+  public isActive(tab: string) {
+    return this.activeTab === tab
+  }
 }
